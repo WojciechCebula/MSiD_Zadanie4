3060420d7187f53a4e8e26b5cb6b32e08a2dd640
WojciechCebula <wojtekcebulaa@gmail.com>
2021-06-13 17:42:39 +0200

    Last

--- filters.py
+++ filters.py
@@ -1,11 +1,9 @@
 import numpy as np
 import cv2
 import multiprocessing
-import pandas as pd
-import time
-from sklearn.decomposition import PCA
 
-def Gabor(image_set, name):
+
+def gabor(image_set, name):
     num = 1
     kernels = []
     queue = multiprocessing.Queue(maxsize=0)
@@ -29,26 +27,3 @@ def apply_kernel(image_set, kernel):
     for image in image_set:
         filtered_images.append(cv2.filter2D(image, cv2.CV_8U, kernel))
     return filtered_images
-
-
-if __name__ == '__main__':
-    from utils import *
-    from save_data import multiprocess_starter
-    import knn
-
-    start_path = './data/fashion/'
-    end_path = './data/filters/train/'
-    # multiprocess_starter(Gabor(load_mnist(start_path, 'train')[0], 'train'), end_path, 8)
-
-    start = time.time()
-    x_train, y_train = load_data('./data/filters/train', 'train', 100)
-    x_test, y_test = load_data('./data/filters/test', 'test', 100)
-    model = knn.model(3, x_train, y_train)
-    x = np.reshape(x_train, (-1, 784))
-    pca = PCA(n_components=2)
-    x = pca.fit_transform(x)
-    model2 = knn.model(3, x, y_train)
-    # score = knn.score(model, x_test, y_test)
-    knn.draw_plot(model2, x_train, y_train)
-    print(time.time() - start)
-    # print(score)
---
+++ main.py
@@ -0,0 +1,32 @@
+import time
+from utils import *
+from save_data import multiprocess_starter
+from filters import *
+from tqdm import tqdm
+import knn
+
+
+def filters_generator():
+    start_path = './data/fashion/'
+    end_path = './data/filters/train/'
+    multiprocess_starter(gabor(load_mnist(start_path, 'train')[0], 'train'), end_path, 8)
+
+
+def knn_model(index, k):
+    x_train, y_train = load_data('./data/filters/train', 'train', index)
+    x_test, y_test = load_data('./data/filters/test', 'test', index)
+    model = knn.model(k, x_train, y_train)
+    return knn.score(model, x_test, y_test)
+
+
+def all_knn_models(start, end, k_array):
+    scores = []
+    i = start
+    for i in tqdm(range(end)):
+        for k in k_array:
+            scores.append((knn_model(start, k), i, k))
+    return scores
+
+
+if __name__ == '__main__':
+    print(all_knn_models(1, 3, [3, 5]))
